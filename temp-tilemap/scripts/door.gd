f23a531cd9be58c0312284ad92d7faa91031f30e
TheJohnDarksoul <156362113+TheJohnDarksoul@users.noreply.github.com>
2024-10-12 14:48:30 -0400

    Doors work

--- temp-tilemap/scripts/door.gd
+++ temp-tilemap/scripts/door.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
+##Scene file to be transitioned to
 @export var targetScene:String = "node_2d.tscn"
+##The player's coordinates in the new scene
 @export var targetPosition: Vector2 = Vector2(0, 0)
 
 func touchingDoor(body: Node2D) -> void:
+	#print("touch")
 	if body.is_in_group("player_group"):
-		GlobalValues.PlayerSpawnPos = targetPosition
-		get_tree().change_scene_to_file("res://scenes/" + targetScene)
-		pass
-	pass # Replace with function body.
+		changeScene.call_deferred()
+		
+func changeScene():
+	GlobalValues.PlayerSpawnPos = targetPosition
+	targetScene = "res://scenes/" + targetScene
+	get_tree().change_scene_to_file(targetScene)
