6c6251e461c2f3f4147494885b8b5d390d98dc43
TheJohnDarksoul <kyle.bacon10@gmail.com>
2024-10-12 21:51:53 -0400

    Created generic item scene

--- temp-tilemap/scripts/door.gd
+++ temp-tilemap/scripts/door.gd
@@ -7,14 +7,27 @@ extends Area2D
 ##The event flags in which the door appears
 @export var activeNumbers: Array = [0]
 
+var touching: bool = false
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("p_interact"):
+		if touching: 
+			for i in range(activeNumbers.size()):
+				if activeNumbers[i] == GlobalValues.EventFlagNum:
+					changeScene.call_deferred()
+			
+	pass
+
 func touchingDoor(body: Node2D) -> void:
 	#print("touch")
 	if body.is_in_group("player_group"):
-		for i in range(activeNumbers.size()):
-			if activeNumbers[i] == GlobalValues.EventFlagNum:
-				changeScene.call_deferred()
+		touching = true
+		
 		
 func changeScene():
 	GlobalValues.PlayerSpawnPos = targetPosition
 	targetScene = "res://scenes/" + targetScene
 	get_tree().change_scene_to_file(targetScene)
+
+func stopTouchingDoor(body: Node2D) -> void:
+	touching = false
